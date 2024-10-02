deb11c704e68fcf5d78cf72011a9dfeb7f83769a
MancSolutions <kenan.marshall@apprentice.ua92.ac.uk>
2024-10-02 13:30:32 +0100

    Update initalsetup.sh
    Repo clone comment

--- initalsetup.sh
+++ initalsetup.sh
@@ -2,6 +2,7 @@
 #This updates Ubuntu 
 sudo apt -y update
 sudo apt -y upgrade
+# This clones my repo
 sudo git clone https://github.com/MancSolutions/Wordpress_project.git /root/wordpress-app
 sudo chmod -R 755 /root/wordpress-app
 # sudo bash /root/wordpress-app/wordpress-install.sh
